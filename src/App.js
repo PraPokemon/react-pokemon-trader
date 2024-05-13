82efc034b5bf48e35b72fc8528a75df19ea8e131
unknown <markovic.maks@gmail.com>
2024-05-13 14:30:06 +0200

    test commit max 2

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload my ass.
         </p>
         <a
           className="App-link"
@@ -17,8 +17,8 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
-     <h1>Pokemon Trader</h1>
+      </header>
+     <h1>Pokemon Traderr</h1>
     </div>
   );
 }
